1bcb9c39a82c45e951c61b8804645e5df10ac1f7
JeremyHu <huquancheng329@gmail.com>
2021-01-11 18:04:37 +0800

    initial commit

--- src/main/java/cn/sakuratown/jeremyhu/customitems/CustomItems.java
+++ src/main/java/cn/sakuratown/jeremyhu/customitems/CustomItems.java
@@ -2,7 +2,14 @@ package cn.sakuratown.jeremyhu.customitems;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * 插件主类
+ * @author JeremyHu
+ */
+
 public class CustomItems extends JavaPlugin {
+
+    @Override
     public void onEnable(){
 
     }
